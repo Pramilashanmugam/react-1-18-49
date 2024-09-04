ec573ed274e325335098ff8259d0e0f8a5c564b5
Pramila Shanmugam <pramila.sl@gmail.com>
2024-09-04 10:59:03 +0000

    intial commit with two components built

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import EventsClass from "./component/EventsClass.js";
+import EventsFunctional from "./component/EventsFunctional.js";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <EventsFunctional />
+    <EventsClass />
+  </div>;
 }
 
 export default App;
